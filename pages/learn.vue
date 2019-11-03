dd732adf404c2eb0ef0d27762e3768d4385c2c92
Samuel Austen <furneauxsam1994@gmail.com>
2019-11-03 20:57:47 +0100

    Added learn page with mdi icon instructions

---
+++ pages/learn.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<v-card>
+			<v-card-title primary-title></v-card-title>
+			<v-card-text>
+				<div>
+					<h3 class="headline mb-0">Using icons</h3>
+					<div>
+						Nuxt comes packaged with Googles Material Design Icons, abbreviated
+						as 'mdi', we must use a v-icon element and insert the name of the icon want
+						to use inside the elements body
+					</div>
+				</div>
+			</v-card-text>
+			<v-card-actions>
+				<v-btn target="#" href="https://cdn.materialdesignicons.com/4.5.95/" color="success">Icon picker</v-btn>
+			</v-card-actions>
+		</v-card>
+	</div>
+</template>
