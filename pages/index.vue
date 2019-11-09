0ede792570200d9a09adbeba53a0a9484836b61b
Goldenfaeron <tobias@mreich.de>
2019-11-09 16:21:30 +0100

    added a example component

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,64 @@
 <template>
-	<v-container grid-list-xs>
-		<v-layout column justify-center align-center>
-			<!-- Carousel  -->
-			<v-flex xs12 sm8 md6 my-5>
-				<v-carousel cycle>
-					<v-carousel-item :src="require('@/assets/camping.jpg')"></v-carousel-item>
-					<v-carousel-item :src="require('@/assets/bike.jpg')"></v-carousel-item>
-					<v-carousel-item :src="require('@/assets/mine.jpg')"></v-carousel-item>
-					<v-carousel-item :src="require('@/assets/river.jpg')"></v-carousel-item>
-				</v-carousel>
-			</v-flex>
-
-			<v-flex xs12 sm8 md6>
-				<div class="text-center">
-					<logo />
-					<vuetify-logo />
-				</div>
-				<v-card>
-					<v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template (No Tobi did thissssssssss)</v-card-title>
-					<v-card-text>
-						<p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-						<p>
-							For more information on Vuetify, check out the
-							<a
-								href="https://vuetifyjs.com"
-								target="_blank"
-							>documentation</a>.
-						</p>
-						<p>
-							If you have questions, please join the official
-							<a
-								href="https://chat.vuetifyjs.com/"
-								target="_blank"
-								title="chat"
-							>discord</a>.
-						</p>
-						<p>
-							Find a bug? Report it on the github
-							<a
-								href="https://github.com/vuetifyjs/vuetify/issues"
-								target="_blank"
-								title="contribute"
-							>issue board</a>.
-						</p>
-						<p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-						<div class="text-xs-right">
-							<em>
-								<small>&mdash; John Leider</small>
-							</em>
-						</div>
-						<hr class="my-3" />
-						<a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-						<br />
-						<a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
-					</v-card-text>
-					<v-card-actions>
-						<v-spacer />
-						<v-btn color="primary" nuxt to="/inspire">Continue</v-btn>
-					</v-card-actions>
-				</v-card>
-			</v-flex>
-		</v-layout>
-	</v-container>
+  <v-hover>
+    <v-card
+      slot-scope="{ hover }"
+      class="mx-auto"
+      color="grey lighten-4"
+      max-width="600"
+    >
+      <v-img
+        :aspect-ratio="16/9"
+        src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
+      >
+        <v-expand-transition>
+          <div
+            v-if="hover"
+            class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal display-3 white--text"
+            style="height: 100%;"
+          >
+            $14.99
+          </div>
+        </v-expand-transition>
+      </v-img>
+      <v-card-text
+        class="pt-4"
+        style="position: relative;"
+      >
+        <v-btn
+          absolute
+          color="orange"
+          class="white--text"
+          fab
+          large
+          right
+          top
+        >
+          <v-icon>mdi-cart</v-icon>
+        </v-btn>
+        <div class="font-weight-light grey--text title mb-2">For the perfect meal</div>
+        <h3 class="display-1 font-weight-light orange--text mb-2">QW cooking utensils</h3>
+        <div class="font-weight-light title mb-2">
+          Our Vintage kitchen utensils delight any chef.<br>
+          Made of bamboo by hand
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-hover>
 </template>
 
-<script>
-import Logo from "~/components/Logo.vue";
-import VuetifyLogo from "~/components/VuetifyLogo.vue";
+<style>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .5;
+  position: absolute;
+  width: 100%;
+}
+</style>
 
+<script>
 export default {
-	components: {
-		Logo,
-		VuetifyLogo
-	}
-};
+
+}
 </script>
