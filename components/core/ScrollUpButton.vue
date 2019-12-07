c4a7ee70abbd8c7952243c3d675dd1cd55d839ea
Samuel Austen <furneauxsam1994@gmail.com>
2019-12-07 13:20:11 +0100

    New Components

---
+++ components/core/ScrollUpButton.vue
@@ -0,0 +1,44 @@
+<template>
+	<v-flex>
+		<v-btn
+			transition="fade-transition"
+			@click="top()"
+			v-scroll="onScroll"
+			v-show="scrollButton"
+			color="secondary"
+			large
+			fixed
+			bottom
+			right
+		>
+			Back to Top
+			<v-icon large>mdi-chevron-up</v-icon>
+		</v-btn>
+	</v-flex>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			scrollButton: false
+		};
+	},
+
+	methods: {
+		top() {
+			window.scrollTo({
+				top: 0,
+				left: 0,
+				behavior: "smooth"
+			});
+		},
+
+		onScroll(e) {
+			if (typeof window === "undefined") return;
+			const top = window.pageYOffset || e.target.scrollTop || 0;
+			this.scrollButton = top > 20;
+		}
+	}
+};
+</script>
